e3ef82ae4e487a7e0785ec644ce3a80a5190477b
caiismyname <davidcai2012@gmail.com>
2018-01-21 00:13:44 -0600

    backend written (connected with twilio and firebase), no authenticaion, hard coded for only seibel

--- parse.py
+++ parse.py
@@ -1,5 +1,9 @@
 import urllib2
 from twilio.rest import Client
+import requests
+import json
+
+serveries = ["Seibel", "North", "Baker", "SidRich", "South", "West"]
 
 class Servery:
 	def __init__(self, name):
@@ -30,7 +34,7 @@ class Servery:
 		return self.menuItems
 
 	def __str__(self):
-		return str(self.name) + ": " + str(self.menuItems)[1:-1]
+		return str(self.name) + ": " + str(self.menuItems)[1:-1].replace("'", "")
 
 ################
 # Twilio
@@ -43,39 +47,100 @@ twilioPhoneNumber = "+17137144366"
 client = Client(twilioAccountSid, twilioAccountToken)
 
 def sendMessage(recipient, message):
+	if recipient[:2] != "+1":
+		recipient = "+1" + recipient
+
 	client.api.account.messages.create(
-	    to="+1" + recipient,
+	    to=recipient,
 	    from_=twilioPhoneNumber,
 	    body=message)
 
+	print recipient, message
 
+
+################
+# Firebase
+################
+
+def addUser(number, servery):
+
+	if 'w' in servery:
+		servery = "West"
+	elif 'bel' in servery:
+		servery = "Seibel"
+	elif 'n' in servery:
+		servery = "North"
+	elif 'k' in servery:
+		servery = "Baker"
+	elif 'ri' in servery:
+		servery = "SidRich"
+	elif 'ou' in servery:
+		servery = "South"
+	else:
+		return False
+
+
+	r = requests.put("https://servery-cef7b.firebaseio.com/serveries/" + servery + ".json", data='{"+1' + str(number) + '":"+1' + str(number) + '"}') # Data confroms to {"key":"value"}, as a string
+	r2 = requests.put("https://servery-cef7b.firebaseio.com/users.json", data='{"+1' + str(number) + '":"' + servery + '"}')
+
+	print r, r2
+
+def getUsersOfServery(servery):
+	r = requests.get("https://servery-cef7b.firebaseio.com/serveries/" + servery + ".json")
+	users = json.loads(r.text).keys()
+
+	print servery, str(users)
+	return users
 
 ################
 # Web scraping
 ################
 
-# Pull webpage
-diningSite = urllib2.urlopen("http://dining.rice.edu").read()
+def getWebsite():
+	# Pull webpage
+	diningSite = urllib2.urlopen("http://dining.rice.edu").read()
 
-# Get menu portion
-startIndex = diningSite.find("<div id=\"main\">")
-endIndex = diningSite.find("<!--//End Main-->")
-main = diningSite[startIndex:endIndex]
+	# Get menu portion
+	startIndex = diningSite.find("<div id=\"main\">")
+	endIndex = diningSite.find("<!--//End Main-->")
+	site = diningSite[startIndex:endIndex]
 
-serveries = ["Seibel", "North", "Baker", "SidRich", "South", "West"]
-menuMap = {}
+	return site
 
-# Find inidividual menus
-for s in serveries:
-	servery = Servery(s)
+def getMenusFromSite(site):
+	menuMap = {}
 
-	startIndex = main.find("<div class=\"servery-title\" id=\"" + s)
-	endIndex = main.find("<div class=\"servery-title\"", startIndex + 1)
-	servery.setHTMLMenu(main[startIndex:endIndex])
+	# Find inidividual menus
+	for s in serveries:
+		servery = Servery(s)
 
-	menuMap[s] = servery
+		startIndex = site.find("<div class=\"servery-title\" id=\"" + s)
+		endIndex = site.find("<div class=\"servery-title\"", startIndex + 1)
+		servery.setHTMLMenu(site[startIndex:endIndex])
+
+		menuMap[s] = servery
+		print servery
+
+	return menuMap
 
 
-print menuMap["North"]
+################
+# Put it all together
+################
+
+def update():
+	menuMap = getMenusFromSite(getWebsite())
+
+	for servery in serveries:
+		if servery == "Seibel":
+			menu = menuMap[servery]
+			users = getUsersOfServery(servery)
+
+			for user in users:
+				sendMessage(user, str(menu))
+
+
+update()
+
 
 
