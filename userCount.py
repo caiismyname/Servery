3c45912a91a39d27b20be029622d740b3f52381a
caiismyname <davidcai2012@gmail.com>
2018-02-06 18:58:35 -0600

    added script to count users

---
+++ userCount.py
@@ -0,0 +1,49 @@
+import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from dotenv import load_dotenv, find_dotenv
+import os
+
+
+
+def initFirebase():
+	# This is so stupid.
+	# Unadultered, the private_key gets ready out with "\\\n" instead of newlines.
+	# Putting the key in surrounded by quotes makes it "\n" (literal).
+		# Note that the surrounding quotes are only needed on my local machine, not on heroku.
+	# Read that, split, then concat actual newlines, to make it a valid private_key.
+
+	privateKeySplit = os.environ.get("FIREBASE-PRIVATE-KEY").split("\\n")
+	privateKey = ""
+	for portion in privateKeySplit:
+		privateKey += portion + "\n"
+
+	serviceAccountKey = {
+		'type': os.environ.get("FIREBASE-TYPE"),
+		# 'project_id': os.environ.get("FIREBASE-PROJECT-ID"), 
+		# 'private_key_id': os.environ.get("FIREBASE-PRIVATE-KEY-ID"),
+		'private_key': privateKey,
+		'client_email': os.environ.get("FIREBASE-CLIENT-EMAIL"), 
+		# 'client_id': os.environ.get("FIREBASE-CLIENT-ID"),
+		# 'auth_uri': os.environ.get("FIREBASE-AUTH-URI"),
+		'token_uri': os.environ.get("FIREBASE-TOKEN-URI"),
+		# 'auth_provider_x509_cert_url': os.environ.get("FIREBASE-AUTH-PROVIDER"),
+		# 'client_x509_cert_url': os.environ.get("FIREBASE-CLIENT-CERT-URL")
+	}
+
+	cred = credentials.Certificate(serviceAccountKey)
+	firebase_admin.initialize_app(cred, {"databaseURL": "https://servery-cef7b.firebaseio.com"})
+
+
+
+def initEnviron():
+	load_dotenv(find_dotenv())
+	initFirebase()
+
+def countAllUsers():
+	ref = db.reference("users/")
+	print(str(len(ref.get().keys())) + " users")
+
+initEnviron()
+countAllUsers()
